ad1ba4d9fb0fdff25c57bbf750e3ec82aed25649
Jason Lieu <jasonlieuthefirst@gmail.com>
2018-01-14 01:55:52 -0800

    Working GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -32,11 +32,23 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
     private int swingTimer;
 
     Drawable drawable = getResources().getDrawable( R.drawable.buttonx, null );
-    //Drawable swingAnimation = getResources().getDrawable(R.drawable.swingAnimation_01);
+    Drawable swingAnimation;
+
+    Drawable hammerAnimation_0 = getResources().getDrawable( R.drawable.hammeranimation0, null );
+    Drawable hammerAnimation_1 = getResources().getDrawable( R.drawable.hammeranimation1, null );
+    Drawable hammerAnimation_2 = getResources().getDrawable( R.drawable.hammeranimation2, null );
+    Drawable hammerAnimation_3 = getResources().getDrawable( R.drawable.hammeranimation3, null );
+    Drawable hammerAnimation_4 = getResources().getDrawable( R.drawable.hammeranimation4, null );
+    Drawable hammerAnimation_5 = getResources().getDrawable( R.drawable.hammeranimation5, null );
+    Drawable hammerAnimation_6 = getResources().getDrawable( R.drawable.hammeranimation6, null );
+    Drawable hammerAnimation_7 = getResources().getDrawable( R.drawable.hammeranimation7, null );
 
 
-    private int swingSpeed = 10;                //CHANGE SWING TIME HERE
 
+
+
+    private int swingSpeed = 7;                //CHANGE SWING TIME HERE
+                                                //ALSO NEEDS TO CHANGE IN SWITCH/CASE in draw
 
     public GamePanel(Context context){
         super(context);
@@ -52,7 +64,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
         smash = false;
 
         pointCount = 0;
-        smashTimer = randomNumber.nextInt((200 - 100) + 1) + 100;
+        smashTimer = randomNumber.nextInt((200 - 100) + 1) + 100;       //  ((max - min) + 1) + min
         swingTimer = swingSpeed;
         text.setColor(Color.BLACK);
         text.setTextSize(100);
@@ -61,11 +73,17 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
         resetText.setTextSize(50);
         buttonLook.setColor(Color.GRAY);
 
-
-
-
-
         drawable.setBounds((width/2 - 100), (int)(height * 0.7 - 100), (width/2 + 100), (int)(height * 0.7 + 100));
+
+
+        hammerAnimation_0.setBounds((int)(width * 0.4), (int)(height * 0.2), (int)(width * 1.3), (int)(height * 0.7));
+        hammerAnimation_1.setBounds((int)(width * 0.4), (int)(height * 0.2), (int)(width * 1.3), (int)(height * 0.7));
+        hammerAnimation_2.setBounds((int)(width * 0.4), (int)(height * 0.2), (int)(width * 1.3), (int)(height * 0.7));
+        hammerAnimation_3.setBounds((int)(width * 0.4), (int)(height * 0.2), (int)(width * 1.3), (int)(height * 0.7));
+        hammerAnimation_4.setBounds((int)(width * 0.4), (int)(height * 0.2), (int)(width * 1.3), (int)(height * 0.7));
+        hammerAnimation_5.setBounds((int)(width * 0.4), (int)(height * 0.2), (int)(width * 1.3), (int)(height * 0.7));
+        hammerAnimation_6.setBounds((int)(width * 0.4), (int)(height * 0.2), (int)(width * 1.3), (int)(height * 0.7));
+        hammerAnimation_7.setBounds((int)(width * 0.4), (int)(height * 0.2), (int)(width * 1.3), (int)(height * 0.7));
 
 
 
@@ -93,6 +111,12 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
     public boolean onTouchEvent(MotionEvent event){
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                if((int) event.getX() >= (width * 0.83 - 100) &&                  //reset button location
+                        (int) event.getX() <= (width * 0.83 + 100) &&              //resets the game
+                        (int) event.getY() >= (height * 0.95 - 50) &&               //calls resetGame()
+                        (int) event.getY() <= (height * 0.95 + 50)){
+                    resetGame();
+                }
             case MotionEvent.ACTION_MOVE:
                 if((int) event.getX() >= (width/2 - 100) &&                     //button location
                         (int) event.getX() <= (width/2 + 100) &&                //press button
@@ -102,14 +126,12 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
                     touch = true;
                 }
                 else {
-                    touch = false;
+                    if(touch == true){
+                        clicked = true;
+                        touch = false;
+                    }
                 }
-                if((int) event.getX() >= (width * 0.83 - 100) &&                  //reset button location
-                        (int) event.getX() <= (width * 0.83 + 100) &&              //resets the game
-                        (int) event.getY() >= (height * 0.95 - 50) &&               //calls resetGame()
-                        (int) event.getY() <= (height * 0.95 + 50)){
-                    resetGame();
-                }
+
                 break;
             case MotionEvent.ACTION_UP:
                 if(touch == true){
@@ -129,48 +151,13 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
             }
         }
         if(swing){
-          /*  switch(swingTimer) {
-                case 10:
-                    swingAnimation = swingAnimation = getResources().getDrawable(R.drawable.swingAnimation_01);
-                    break;
-                case 9:
-                    swingAnimation = swingAnimation = getResources().getDrawable(R.drawable.swingAnimation_01);
-                    break;
-                case 8:
-                    swingAnimation = swingAnimation = getResources().getDrawable(R.drawable.swingAnimation_01);
-                    break;
-                case 7:
-                    swingAnimation = swingAnimation = getResources().getDrawable(R.drawable.swingAnimation_01);
-                    break;
-                case 6:
-                    swingAnimation = swingAnimation = getResources().getDrawable(R.drawable.swingAnimation_01);
-                    break;
-                case 5:
-                    swingAnimation = swingAnimation = getResources().getDrawable(R.drawable.swingAnimation_01);
-                    break;
-                case 4:
-                    swingAnimation = swingAnimation = getResources().getDrawable(R.drawable.swingAnimation_01);
-                    break;
-                case 3:
-                    swingAnimation = swingAnimation = getResources().getDrawable(R.drawable.swingAnimation_01);
-                    break;
-                case 2:
-                    swingAnimation = swingAnimation = getResources().getDrawable(R.drawable.swingAnimation_01);
-                    break;
-                case 1:
-                    swingAnimation = swingAnimation = getResources().getDrawable(R.drawable.swingAnimation_01);
-                    break;
-                case 0:
-                    swingAnimation = swingAnimation = getResources().getDrawable(R.drawable.swingAnimation_01);
-                    break;
-            }*/
             swingTimer--;                           //animate here
             if(swingTimer == 0){
                 smash = true;
                 swing = false;
             }
         }
-        if(touch && smash){                         //get hit
+        if(touch && smash && !win){                         //get hit
             lose = true;
             pointCount = 0;
         }
@@ -199,10 +186,37 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
     public void draw(Canvas canvas){
         super.draw(canvas);
         canvas.drawColor(Color.WHITE);
+        drawable.draw(canvas);
+        switch(swingTimer){
+            case 7:
+                hammerAnimation_0.draw(canvas);
+                break;
+            case 6:
+                hammerAnimation_1.draw(canvas);
+                break;
+            case 5:
+                hammerAnimation_2.draw(canvas);
+                break;
+            case 4:
+                hammerAnimation_3.draw(canvas);
+                break;
+            case 3:
+                hammerAnimation_4.draw(canvas);
+                break;
+            case 2:
+                hammerAnimation_5.draw(canvas);
+                break;
+            case 1:
+                hammerAnimation_6.draw(canvas);
+                break;
+            case 0:
+                hammerAnimation_7.draw(canvas);
+                break;
+        }
         canvas.drawText("Score: " + pointCount/10,((width/2) - 185),(int)(height * 0.25), text);
         canvas.drawText("RESET",(int)(width * 0.83),(int)(height * 0.95), resetText);
-        canvas.drawText("smash: " + smashTimer,(width/2),(height/2)+ 100, text);                    //temp
-        canvas.drawText("swing: " + swingTimer,(width/2),(height/2)+ 300, text);                    //temp
+        //canvas.drawText("smash: " + smashTimer,(int)(width*0.15),(height/2)+ 100, text);                    //temp
+        //canvas.drawText("swing: " + swingTimer,(int)(width*0.15),(height/2)+ 300, text);                    //temp
         if(win){
             canvas.drawText("win",(width/2) - 80,(int)(height * 0.2), text);
         }
@@ -212,8 +226,6 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
         if(tooSoon){
             canvas.drawText("too soon",(width/2) - 80,(int)(height * 0.2), text);
         }
-        drawable.draw(canvas);
-        //swingAnimation.draw(canvas);
     }
 
 
